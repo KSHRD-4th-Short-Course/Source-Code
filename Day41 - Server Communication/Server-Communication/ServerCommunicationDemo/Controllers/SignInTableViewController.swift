53a58dbbbb4ca19cf70647c7f4a3ba3863474424
Kokpheng <yinkokpheng@yahoo.com>
2017-12-18 17:41:49 +0700

    Add source code

---
+++ Day41 - Server Communication/Server-Communication/ServerCommunicationDemo/Controllers/SignInTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignInTableViewController.swift
+//  ServerCommunicationDemo
+//
+//  Created by Kokpheng on 11/10/16.
+//  Copyright © 2016 Kokpheng. All rights reserved.
+//
+
+import UIKit
+
+class SignInTableViewController: UITableViewController {
+    
+    // Outlet
+    @IBOutlet var emailTextField : UITextField!
+    @IBOutlet var passwordTextField : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // TODO: SignIn IBAction
+    @IBAction func signInAction(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Login with Facebook
+    @IBAction func loginWithFacebook(_ sender: Any) {
+    
+    }
+}
